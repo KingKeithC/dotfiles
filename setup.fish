25e7cf5bdcf44a4b6ffeec1df881ac0d8b43bf43
Keith King <KingKeithC@gmail.com>
2020-06-28 19:56:14 -0600

    Add Distro setup for Ubuntu

--- setup.fish
+++ setup.fish
@@ -3,6 +3,22 @@
 # Set the Config Directory to the directory of thie script.
 set CONFIG_DIR (dirname (readlink -m (status --current-filename)))
 
+# Install Distro Specific Packages
+set DISTRO (cat /etc/*-release | grep -oP 'DISTRIB_ID=\K.*')
+
+echo "Performing Setup for DISTRO=$DISTRO"
+
+switch $DISTRO
+case Ubuntu
+  set PACKAGES "vim less git"
+  sudo apt-get update -y
+  sudo apt-get upgrade -y
+  sudo apt-get install -y $PACKAGES
+  
+  set -x EDITOR vim
+  set -x PAGER less
+end
+
 # Define a function to link a file into home from the config directory
 function link
   set TARGET $CONFIG_DIR/$argv[2]
