18c8d5c57cf015b611dfcf23edc7ef744b796c90
Keith King <KingKeithC@gmail.com>
2023-11-11 17:04:14 -0700

    Add i3 config

--- setup.sh
+++ setup.sh
@@ -9,14 +9,31 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DOTFILES="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
+LINK_CMD="ln -vs --backup=numbered"
+
+# Default Configs
+echo "=== Configuring Defaults ==="
+
+echo "-- Installing Oh-my-bash --"
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
 
+echo "-- Linking Dotfiles --"
 mkdir -p ~/.ssh ~/.gnupg
+$LINK_CMD "$DOTFILES/bash_profile.sh" ~/.bash_profile
+$LINK_CMD "$DOTFILES/bashrc.sh"       ~/.bashrc
+$LINK_CMD "$DOTFILES/gitconfig"       ~/.gitconfig
+$LINK_CMD "$DOTFILES/vimrc"           ~/.vimrc
+$LINK_CMD "$DOTFILES/ssh-config"      ~/.ssh/config
+$LINK_CMD "$DOTFILES/gpg.conf"        ~/.gnupg/gpg.conf
+$LINK_CMD "$DOTFILES/gpg-agent.conf"  ~/.gnupg/gpg-agent.conf
 
-ln -vs --backup=numbered "$DOTFILES/bash_profile.sh" ~/.bash_profile
-ln -vs --backup=numbered "$DOTFILES/bashrc.sh"       ~/.bashrc
-ln -vs --backup=numbered "$DOTFILES/gitconfig"       ~/.gitconfig
-ln -vs --backup=numbered "$DOTFILES/vimrc"           ~/.vimrc
-ln -vs --backup=numbered "$DOTFILES/ssh-config"      ~/.ssh/config
-ln -vs --backup=numbered "$DOTFILES/gpg.conf"        ~/.gnupg/gpg.conf
-ln -vs --backup=numbered "$DOTFILES/gpg-agent.conf"  ~/.gnupg/gpg-agent.conf
+
+# i3 Config
+if [[ -n "$SETUP_i3" ]]; then
+  echo "=== Configuring i3 ==="
+  mkdir -p ~/.config/i3
+  sudo mv -vf /etc/i3/config /etc/i3/config-default 2> /dev/null
+
+  $LINK_CMD "$DOTFILES/i3-config" ~/.config/i3/config
+  $LINK_CMD "$DOTFILES/i3-status" ~/.config/i3status/config
+fi
